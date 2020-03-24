e059e33c1b92fe3afae95610173f77c720eb1830
Shaimaa <shaimaa333salama@gmail.com>
2020-03-24 22:03:25 +0200

    youtube

---
+++ app/src/main/java/com/google/youtubechannel/UI/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.google.youtubechannel.UI.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.youtubechannel.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
